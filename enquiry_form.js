61473ed1813a57994c0923ddd6e87b13dc25914c
Shraddha82001 <shraddhaj478@gmail.com>
2025-05-02 23:53:00 +0530

    Add new chnages of javascript

--- enquiry_form.js
+++ enquiry_form.js
@@ -1,13 +1,41 @@
+var username = [];
+var email = [];
+var phone = [];
+var enquiry = [];
+var message = [];
+
+var row = 1;
+
 function callvalue(){
-    var name=document.getElementById("username").value;
+    var username=document.getElementById("username").value;
     var email=document.getElementById("email").value;
     var phone=document.getElementById("phone").value;
+    var enquiry=document.getElementById("enquiry").value;
     var message=document.getElementById("message").value;
 
+    // Reference to table body
+    var callvalue = document.getElementById('display');
 
-    document.writeln("your info : <br> "+" Your name: "+name+" <br>");
-    document.writeln(" Your email: "+email+" <br>" );
-    document.writeln(" Your phone: "+phone+" <br>" );
-    document.writeln(" Your message: "+message);
+    var NewRow = callvalue.insertRow(row);
 
-}
+    var cell1 = NewRow.insertCell(0);
+    var cell2 = NewRow.insertCell(1);
+    var cell3 = NewRow.insertCell(2);
+    var cell4 = NewRow.insertCell(3);
+    var cell5 = NewRow.insertCell(4);
+
+    cell1.innerHTML = username;
+    cell2.innerHTML = email;
+    cell3.innerHTML = phone;
+    cell4.innerHTML = enquiry;
+    cell5.innerHTML = message;
+
+    row++;
+ 
+
+    document.getElementById("username").value = '';
+    document.getElementById("email").value = '';
+    document.getElementById("phone").value = '';
+    document.getElementById("enquiry").value = '';
+    document.getElementById("message").value = '';
+} 
